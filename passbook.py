07bd5928656a680b2236f02005242aacd745fd80
vishu jain <vishu@vishu-Vostro-3446.(none)>
2015-03-17 17:57:08 +0530

    Designed view for nereid passbook #7319

--- passbook.py
+++ passbook.py
@@ -37,7 +37,7 @@ class Pass(ModelSQL, ModelView):
     )
 
     registrations = fields.One2Many(
-        'nereid.passbook.registration', 'pass', 'Registrations'
+        'nereid.passbook.registration', 'pass_', 'Registrations'
     )
 
     last_update = fields.Function(
